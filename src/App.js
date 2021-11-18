f525d5fc8ca97b8244cc81a37a29918404a7522a
funnykoss <75586141+funnykoss@users.noreply.github.com>
2021-11-18 16:13:29 +0200

    stopwatch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
+import React, { useState, useMemo, useCallback, useEffect } from 'react';
+import { Controls } from './components/Controls';
+import { Observable, Subject } from 'rxjs';
+import { map, buffer, debounceTime, filter, takeUntil } from 'rxjs/operators';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState('stop');
+  const [time, setTime] = useState(0);
+
+  const stop$ = useMemo(() => new Subject(), []);
+  const click$ = useMemo(() => new Subject(), []);
+
+  const start = () => {
+    setState('start');
+  };
+
+  const stop = useCallback(() => {
+    setTime(0);
+    setState('stop');
+  }, []);
+
+  const reset = useCallback(() => {
+    setTime(0);
+  }, []);
+
+  const wait = useCallback(() => {
+    click$.next();
+    setState('wait');
+    click$.next();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    const doubleClick$ = click$.pipe(
+      buffer(click$.pipe(debounceTime(300))),
+      map(list => list.length),
+      filter(value => value >= 2),
+    );
+    const timer$ = new Observable(observer => {
+      let count = 0;
+      const intervalId = setInterval(() => {
+        observer.next((count += 1));
+        console.log(count);
+      }, 1000);
+
+      return () => {
+        clearInterval(intervalId);
+      };
+    });
+
+    const subscribtion$ = timer$
+      .pipe(takeUntil(doubleClick$))
+      .pipe(takeUntil(stop$))
+      .subscribe({
+        next: () => {
+          if (state === 'start') {
+            setTime(prev => prev + 1);
+          }
+        },
+      });
+
+    return () => {
+      subscribtion$.unsubscribe();
+    };
+  }, [state]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <Controls
+        time={time}
+        start={start}
+        stop={stop}
+        reset={reset}
+        wait={wait}
+      />
+    </section>
   );
 }
 
